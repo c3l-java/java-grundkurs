d14554a6048802a2e284e27b294357c72fa96697
tablackmore <tablackmore@gmail.com>
2013-11-13 15:48:19 +0100

    Added remaining completed exercises

---
+++ lectures/week2/day3_Recap/completed_exercises/point/Point.java
@@ -0,0 +1,14 @@
+class Point {
+	double x;
+	double y;
+	Point(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	int getDistance(Point point){
+		double width, height;
+		width = point.x  - this.x;
+		height = point.y - this.y;
+		return (int)Math.sqrt((width * width) + (height * height));
+	}
+}
